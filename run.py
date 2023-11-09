a3e0745919c2225675a21e8883df1bee2574f71a
Nachiket Galande <nachiketgalande1609@gmail.com>
2023-11-09 14:14:54 +0530

    Commented app.run command

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ app = create_app()
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#    app.run(debug=True)
 
 
 # This file is just to run the program
