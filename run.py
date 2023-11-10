2ed11cc474240a148b4bda0e01850062904634b0
Nachiket Galande <nachiketgalande1609@gmail.com>
2023-11-10 15:17:32 +0530

    Created virtualenv to capture correct requirements

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=10000, debug=False)
+    app.run(debug=True)
 
 
 # This file is just to run the program
